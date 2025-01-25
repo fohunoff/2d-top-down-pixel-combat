7544e371df57f6c814061c7be748cc281f8c40ef
Ivan Zagainov <fohunoff@gmail.com>
2025-01-25 20:45:36 +0100

    magic laser (grow and fade)

--- Assets/Scripts/Inventory/Staff.cs
+++ Assets/Scripts/Inventory/Staff.cs
@@ -31,6 +31,7 @@ public class Staff : MonoBehaviour, IWeapon
 
     public void SpawnStaffProjectileAnimEvent() {
         GameObject newLAser = Instantiate(magicLaser, magicLaserSpawnPoint.position, Quaternion.identity);
+        newLAser.GetComponent<MagicLaser>().UpdateLaserRange(weaponInfo.weaponRange);
     }
 
     private void MouseFollowWithOffset() {
